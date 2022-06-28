fef27ceb180538bedf368ffcf3e2d80f6966b1af
r33drichards <xxmrmau5@gmail.com>
2022-06-28 01:16:24 +0000

    add simpler prompt for workspace when workspace is cloned without user-dotbrev

---
+++ ubuntu-proxy/simpler-default-ps1.bash
@@ -0,0 +1,25 @@
+# copied more than i need here, and could omit the force_color_prompt portion
+# of this, but this works just as well
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='\w\$ '
+fi
+unset color_prompt force_color_prompt
